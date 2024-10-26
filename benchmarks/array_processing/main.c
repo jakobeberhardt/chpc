f441ae739a025fe8e77bc94c865184cc31aa935f
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-10-26 16:09:28 +0200

    adds gcc functions

--- benchmarks/array_processing/main.c
+++ benchmarks/array_processing/main.c
@@ -1,94 +1,115 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 
-void predicated_function(int *data, unsigned long size);
-void speculative_function(int *data, unsigned long size);
+void predicated_asm_function(int *data, unsigned long size);
+void speculative_asm_function(int *data, unsigned long size);
+void predicated_gcc_function(int *data, unsigned long size) {
+  for (unsigned long i = 0; i < size; i++) {
+    if (data[i] < 50) {
+      data[i] = 0;
+    }
+  }
+}
+void speculative_gcc_function(int *data, unsigned long size) {
+  for (unsigned long i = 0; i < size; i++) {
+    if (data[i] < 50) {
+      data[i] = 0;
+    }
+  }
+}
+void function(int *data, unsigned long size) {
+  for (unsigned long i = 0; i < size; i++) {
+    if (data[i] < 50) {
+      data[i] = 0;
+    }
+  }
+}
 
 double get_time_in_seconds() {
-    struct timespec t;
-    clock_gettime(CLOCK_MONOTONIC, &t);
-    return t.tv_sec + t.tv_nsec * 1e-9;
+  struct timespec t;
+  clock_gettime(CLOCK_MONOTONIC, &t);
+  return t.tv_sec + t.tv_nsec * 1e-9;
 }
 
 void process_data(int *data, unsigned long size) {
-    #pragma omp parallel for
-    for (unsigned long i = 0; i < size; i++) {
-        if (data[i] < 50) {
-            data[i] = 0;
-        }
+#pragma omp parallel for
+  for (unsigned long i = 0; i < size; i++) {
+    if (data[i] < 50) {
+      data[i] = 0;
     }
+  }
 }
 
 int check_result(int *expected_data, int *actual_data, unsigned long size) {
-    int mismatch = 0;
-    #pragma omp parallel for reduction(|: mismatch)
-    for (unsigned long i = 0; i < size; i++) {
-        if (expected_data[i] != actual_data[i]) {
-            mismatch = 1;
-        }
+  int mismatch = 0;
+#pragma omp parallel for reduction(| : mismatch)
+  for (unsigned long i = 0; i < size; i++) {
+    if (expected_data[i] != actual_data[i]) {
+      mismatch = 1;
     }
-    return !mismatch;
+  }
+  return !mismatch;
 }
 
 int main() {
-    const unsigned long size = 100000000;
-    int *data = malloc(size * sizeof(int));
-    int *expected_data = malloc(size * sizeof(int));
+  const unsigned long size = 100000000;
+  int *data = malloc(size * sizeof(int));
+  int *expected_data = malloc(size * sizeof(int));
 
-    if (!data || !expected_data) {
-        fprintf(stderr, "Memory allocation failed\n");
-        return 1;
+  if (!data || !expected_data) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return 1;
+  }
+
+#pragma omp parallel
+  {
+    unsigned int seed = time(NULL) + omp_get_thread_num();
+#pragma omp for
+    for (unsigned long i = 0; i < size; i++) {
+      data[i] = rand_r(&seed) % 100;
+      expected_data[i] = data[i];
     }
+  }
 
-    #pragma omp parallel
-    {
-        unsigned int seed = time(NULL) + omp_get_thread_num();
-        #pragma omp for
-        for (unsigned long i = 0; i < size; i++) {
-            data[i] = rand_r(&seed) % 100;
-            expected_data[i] = data[i];
-        }
+  double start_time = get_time_in_seconds();
+  predicated_asm_function(data, size);
+  double end_time = get_time_in_seconds();
+  printf("Predicated function took %f seconds\n", end_time - start_time);
+
+  process_data(expected_data, size);
+
+  if (check_result(expected_data, data, size)) {
+    printf("Predicated function produced correct results\n");
+  } else {
+    printf("Predicated function produced incorrect results\n");
+  }
+
+#pragma omp parallel
+  {
+    unsigned int seed = time(NULL) + omp_get_thread_num();
+#pragma omp for
+    for (unsigned long i = 0; i < size; i++) {
+      data[i] = rand_r(&seed) % 100;
+      expected_data[i] = data[i];
     }
+  }
 
-    double start_time = get_time_in_seconds();
-    predicated_function(data, size);
-    double end_time = get_time_in_seconds();
-    printf("Predicated function took %f seconds\n", end_time - start_time);
+  start_time = get_time_in_seconds();
+  speculative_asm_function(data, size);
+  end_time = get_time_in_seconds();
+  printf("Speculative function took %f seconds\n", end_time - start_time);
 
-    process_data(expected_data, size);
+  process_data(expected_data, size);
 
-    if (check_result(expected_data, data, size)) {
-        printf("Predicated function produced correct results\n");
-    } else {
-        printf("Predicated function produced incorrect results\n");
-    }
+  if (check_result(expected_data, data, size)) {
+    printf("Speculative function produced correct results\n");
+  } else {
+    printf("Speculative function produced incorrect results\n");
+  }
 
-    #pragma omp parallel
-    {
-        unsigned int seed = time(NULL) + omp_get_thread_num();
-        #pragma omp for
-        for (unsigned long i = 0; i < size; i++) {
-            data[i] = rand_r(&seed) % 100;
-            expected_data[i] = data[i];
-        }
-    }
-
-    start_time = get_time_in_seconds();
-    speculative_function(data, size);
-    end_time = get_time_in_seconds();
-    printf("Speculative function took %f seconds\n", end_time - start_time);
-
-    process_data(expected_data, size);
-
-    if (check_result(expected_data, data, size)) {
-        printf("Speculative function produced correct results\n");
-    } else {
-        printf("Speculative function produced incorrect results\n");
-    }
-
-    free(data);
-    free(expected_data);
-    return 0;
+  free(data);
+  free(expected_data);
+  return 0;
 }
